92eda600702552ca3b52f133f5a9f07f0c4a6dcd
barni <lesi-szellb.25ad@acsjszki.hu>
2026-03-18 10:35:37 +0100

    szamolas

--- script.js
+++ script.js
@@ -2,4 +2,13 @@
 
 function gomb() {
     document.getElementById("output").innerHTML = "Hello World!";
+
 }
+
+function calculate() {
+    var num1 = parseInt(document.getElementById("num1").value);
+    var num2 = parseFloat(document.getElementById("num2").value);
+    var result = num1 + num2;
+    document.getElementById("result").innerHTML = result;
+}
+
